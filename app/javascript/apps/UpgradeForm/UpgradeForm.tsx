f47f0444269a63dc5227885ce6e422d66ca87642
Jon Allured <jon.allured@gmail.com>
2021-01-05 22:43:53 -0600

    WIP
    Ok I think this is on the right track finally. It'll mean keeping good
    separation between Stripe code and my own, but it seems to be working.
    The next step would be to scrap my testing of the UpgradeForm and drop
    down into the InnerForm. That should allow me to mock stripe by
    simplying passing a prop for the createSource function and then mocking
    both side of returning and throwing.

--- app/javascript/apps/UpgradeForm/UpgradeForm.tsx
+++ app/javascript/apps/UpgradeForm/UpgradeForm.tsx
@@ -1,18 +1,59 @@
 import React from "react"
-import { loadStripe } from "@stripe/stripe-js"
-import { Elements } from "@stripe/react-stripe-js"
-import { InnerForm, InnerFormProps } from "./components/InnerForm"
+import { loadStripe, Source, StripeError } from "@stripe/stripe-js"
+import {
+  CardElement,
+  Elements,
+  useElements,
+  useStripe,
+} from "@stripe/react-stripe-js"
+import { InnerForm } from "./components/InnerForm"
+import { UpgradeFormFetcher } from "./UpgradeFormFetcher"
 
-export interface UpgradeFormProps extends InnerFormProps {
+export interface UpgradeFormProps {
+  email: string
+  fetcher: UpgradeFormFetcher
+  honeybadger: { notify: (any) => void }
   stripePublicKey: string
 }
 
 export const UpgradeForm: React.FC<UpgradeFormProps> = (props) => {
-  const stripePromise = loadStripe(props.stripePublicKey)
+  const { email, fetcher, honeybadger, stripePublicKey } = props
+
+  const stripePromise = loadStripe(stripePublicKey)
+  const stripe = useStripe()
+  const elements = useElements()
+
+  const createSource = (): Promise<{
+    source?: Source
+    error?: StripeError
+  }> => {
+    const element = elements.getElement(CardElement)
+    const sourceData = { type: "card", owner: { email } }
+
+    return stripe.createSource(element, sourceData)
+  }
+
+  const cardStyles = {
+    fontSize: "20px",
+  }
+
+  const cardElementOptions = {
+    style: {
+      base: cardStyles,
+    },
+    hideIcon: true,
+  }
 
   return (
     <Elements stripe={stripePromise}>
-      <InnerForm {...props} />
+      <InnerForm
+        createSource={createSource}
+        disabled={!stripe}
+        fetcher={fetcher}
+        honeybadger={honeybadger}
+      >
+        <CardElement options={cardElementOptions} />
+      </InnerForm>
     </Elements>
   )
 }
